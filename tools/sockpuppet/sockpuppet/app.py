96e886f260dfe0a54a394d82115c34999dd79d9b
Andrew Li <andrewli@u.northwestern.edu>
2026-07-01 21:14:08 -0400

    sockpuppet: Add on_sleep and on_quit hooks to prevent broken system config invariants

--- tools/sockpuppet/sockpuppet/app.py
+++ tools/sockpuppet/sockpuppet/app.py
@@ -11,9 +11,11 @@ class VpnApp(rumps.App):
         self._backend = backend
         super().__init__(backend.format_title(State.DISCONNECTED))
         self.menu = ["Connect", "Disconnect"]
+        rumps.events.on_sleep.register(self._on_sleep)
+        rumps.events.before_quit.register(self._on_quit)
 
     @rumps.clicked("Connect")
-    def connect(self, _):
+    def connect(self, _) -> None:
         self.title = self._backend.format_title(State.CONNECTING)
 
         if self._backend.connect(self._on_unexpected_exit) is Result.FAIL:
@@ -29,12 +31,22 @@ class VpnApp(rumps.App):
         self.title = self._backend.format_title(State.CONNECTED)
 
     @rumps.clicked("Disconnect")
-    def disconnect(self, _):
+    def disconnect(self, _) -> None:
         self._backend.disconnect()
         self._backend.set_proxy(enable=False)
         self.title = self._backend.format_title(State.DISCONNECTED)
         logger.info("Disconnected")
 
     def _on_unexpected_exit(self) -> None:
-        logger.error("Tunnel exited unexpectedly")
+        logger.error("Tunnel exited unexpectedly, resetting proxy")
+        self._backend.set_proxy(enable=False)
         self.title = self._backend.format_title(State.FAILED)
+
+    def _on_sleep(self) -> None:
+        logger.info("System going to sleep, disconnecting")
+        self.disconnect(None)
+
+    def _on_quit(self) -> None:
+        logger.info("Quitting, disconnecting")
+        self._backend.disconnect()
+        self._backend.set_proxy(enable=False)
