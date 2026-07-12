ba7f8c6f8a715e162fec209800333b5a111112d8
Andrew Li <andrewli@u.northwestern.edu>
2026-07-11 22:51:37 -0400

    sockpuppet: Update default config path to be relative to install folder

--- tools/sockpuppet/sockpuppet/__init__.py
+++ tools/sockpuppet/sockpuppet/__init__.py
@@ -4,16 +4,20 @@ from .config import VpnConfig as VpnConfig
 
 def main() -> None:
     import argparse
+    import sys
+    from pathlib import Path
 
     from .backend import SensibleDefaultBackend
+
+    default_config = Path(sys.argv[0]).parent.resolve() / "sockpuppet.toml"
 
     parser = argparse.ArgumentParser(description="sockpuppet SOCKS proxy VPN")
     parser.add_argument(
         "-c",
         "--config",
-        default="config.toml",
+        default=str(default_config),
         metavar="PATH",
-        help="path to config.toml (default: config.toml)",
+        help="path to config (default: sockpuppet.toml next to the executable)",
     )
     args = parser.parse_args()
 
