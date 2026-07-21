a18bd6039d28a8fae36ca5689b874bb87bd9e647
Andrew Li <andrewli@u.northwestern.edu>
2026-07-21 00:35:15 -0400

    smearglev2: Support empty school start year

--- dsl/smeargle/smeargle/blocks/school.py
+++ dsl/smeargle/smeargle/blocks/school.py
@@ -10,20 +10,26 @@ from .degree import Degree
 @dataclass
 class School(Block):
     name: str
-    start: str
+    start: str | None
     until: str
     where: str
     degrees: list[Degree]
     gpa: str | None = None
 
     def _to_typst(self) -> str:
+        def period_expr():
+            if self.start == self.until or self.start == "" or self.start is None:
+                return f"{Formatter.to_typst(self.until)}"
+            assert self.start is not None
+            return f'{Formatter.to_typst(self.start)} + " – " + {Formatter.to_typst(self.until)}'
+
         degree_expr = ' + "; " + '.join(degree.to_typst() for degree in self.degrees) if self.degrees else '""'
-        period_expr = f'{Formatter.to_typst(self.start)} + " – " + {Formatter.to_typst(self.until)}'
         gpa_expr = Formatter.to_typst(self.gpa) if self.gpa else '""'
+
         return (
             "#school(\n"
             f"  {Formatter.to_typst(self.name)},\n"
-            f"  {period_expr},\n"
+            f"  {period_expr()},\n"
             f"  {degree_expr},\n"
             f"  {Formatter.to_typst(self.where)},\n"
             f"  {gpa_expr},\n"
