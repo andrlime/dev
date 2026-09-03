cb5d250ba6452e55378d73d74897e177259fce75
Andrew Li <andrewli@u.northwestern.edu>
2026-09-03 19:24:00 +0800

    dsl/smeargle: Update degree to have customizable separator

--- dsl/smeargle/smeargle/blocks/degree.py
+++ dsl/smeargle/smeargle/blocks/degree.py
@@ -10,9 +10,10 @@ class Degree:
     title: str
     major: str
     note: str | None = None
+    separator: str = " in "
 
     def to_typst(self) -> str:
-        expr = f'{Formatter.to_typst(self.title)} + ", " + {Formatter.to_typst(self.major)}'
+        expr = f'{Formatter.to_typst(self.title)} + "{self.separator}" + {Formatter.to_typst(self.major)}'
         if self.note:
             expr += f' + " (" + {Formatter.to_typst(self.note)} + ")"'
         return expr
